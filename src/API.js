30cf77ebe5098faeef3e23e287f2e3e9f4b07950
Sammi A <sammi.m.yin@gmail.com>
2016-09-28 20:33:15 -0700

    https

--- src/API.js
+++ src/API.js
@@ -3,12 +3,12 @@ import ServerActions from './actions/ServerActions'
 
 const API = {
   getDefaultForecast(value){
-    $.get(`http://api.wunderground.com/api/a98c8087f5b3c5c7/forecast/q/${value}.json`,forecasts => {
+    $.get(`https://api.wunderground.com/api/a98c8087f5b3c5c7/forecast/q/${value}.json`,forecasts => {
       ServerActions.receiveDefaultForecast(forecasts);
     });
   },
   getDefault(value){
-    $.get(`http://api.wunderground.com/api/a98c8087f5b3c5c7/conditions/q/${value}.json`,conditions => {
+    $.get(`https://api.wunderground.com/api/a98c8087f5b3c5c7/conditions/q/${value}.json`,conditions => {
       ServerActions.receiveDefault(conditions);
     });
   }
